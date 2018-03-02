7fca9bdcdd3c834b3fd7ed206507843488175cf4
Willian Z <Willian@Willian-Zhang.com>
2018-03-01 20:17:11 -0500

    just to update

--- labyrinth.js
+++ labyrinth.js
@@ -1,11 +1,29 @@
 'use strict';
-function setup() {    
-    let board = new Board();
+
+function setup() {   
+    let button =  new Button()
+    let devices = [
+        button,
+        new CapasitiveSensor(),
+        new CapasitiveSensor(),
+        new CapasitiveSensor(),
+        new CapasitiveSensor(),
+        new CapasitiveSensor()
+    ]
+    let board = new Board(devices);
     board.connect({baudrate: 9600});
     board.on('connected', e=>console.log(e))
-    board.on('point', (point)=>{
-        console.log(point[6]);
+    // board.on('point', (point)=>{
+    //     console.log(point);
+    // });
+    board.on('ball-in', (which)=>{
+        console.log('ball-in', which);
     });
+    board.on('ball-out', ()=>{
+        console.log('ball-out');
+    });
+    button.on('press', ()=>board.start_reset());
+    button.on('release', ()=>board.finish_reset());
 }
 
 
