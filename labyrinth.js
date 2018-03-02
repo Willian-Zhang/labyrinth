fff3928eab79f91fd1d55a6d70375e7b13520dc7
Willian Z <Willian@Willian-Zhang.com>
2018-03-02 00:33:45 -0500

    clean the code

--- labyrinth.js
+++ labyrinth.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function setup() {   
-    let button =  new Button()
+    let button =  new Button();
     let devices = [
         button,
         new CapasitiveSensor(),
@@ -9,21 +9,24 @@ function setup() {
         new CapasitiveSensor(),
         new CapasitiveSensor(),
         new CapasitiveSensor()
-    ]
+    ];
     let board = new Board(devices);
-    board.connect({baudrate: 9600});
-    board.on('connected', e=>console.log(e))
-    // board.on('point', (point)=>{
-    //     console.log(point);
-    // });
+    let port = board.connect({baudrate: 9600});
+    console.log(`Connecting on ${port}...`)
+    board.on('connected', ()=>{
+        console.log('connected');
+        button.on('press', ()=>board.start_reset());
+        button.on('release', ()=>board.finish_reset());
+    });
+    board.on('warning', (warning)=>{
+        console.warn(warning);
+    });
     board.on('ball-in', (which)=>{
         console.log('ball-in', which);
     });
     board.on('ball-out', ()=>{
         console.log('ball-out');
     });
-    button.on('press', ()=>board.start_reset());
-    button.on('release', ()=>board.finish_reset());
 }
 
 
